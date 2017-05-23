ea5192881a44a3adb6686184953595bd21995657
Hieu <Hieu@hieus-mbp.gateway.sonic.net>
2017-05-23 01:11:47 -0700

    updated volunteer content

--- src/app/pages/volunteers.js
+++ src/app/pages/volunteers.js
@@ -50,14 +50,17 @@ export class Volunteers extends Component {
           <article className="article_part">
             <img src={img2} className="sub_img"/>
             <div className="verticle_line2"/>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At eaque nisi hic nostrum delectus sint, similique perferendis est obcaecati minima aperiam facilis ab quod, ex provident doloribus debitis, ipsum dignissimos.
+            <div>
+              <p>
+              The SF Charity Fashion Show is 100% volunteer-run series of fashion shows to raise funds for charitable causes. As everyone involved is a volunteer, we are able to minimize overhead and donate as much as possible directly to the charity. 100% of net proceeds are donated.
+              </p>
+              <p>Come! Get Involved!</p>
               <div>
                 <a onClick={()=>{this.setState({submit:false})}} href="/designer_volunteers"><button className="volunteer_btn">Designer Volunteer</button></a>
                 <a href="/model_volunteers"><button className="volunteer_btn">Model Volunteer</button></a>
                 <a href="/administrator_volunteers"><button className="volunteer_btn">Administrator Volunteer</button></a>
               </div>
-            </p>
+            </div>
           </article>
         </section>
       </div>
