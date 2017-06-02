115fcd84ad8d49a046f2f3a5b64007123d2f0473
Hieu <Hieu@Hieus-MacBook-Pro.local>
2017-06-01 18:17:59 -0700

    added sub menu feature

--- src/app/nav.js
+++ src/app/nav.js
@@ -15,10 +15,33 @@ export class Nav extends Component {
           <hr/>
           <ul>
             <a href="/"><li className="nav_items">Home</li></a>
-            <a href="/about"><li className="nav_items">About</li></a>
+            <a href="/about"><li className="nav_items">About Us</li></a>
             <a href="/the-issue"><li className="nav_items">The Issue</li></a>
-            <a href="/events"><li className="nav_items">Events</li></a>
-            <a href="/volunteers"><li className="nav_items">Volunteers</li></a>
+            <a href="/events">
+              {/*<input id="events_dropdown" type="checkbox"/>*/}
+              
+                <li className="nav_items">
+                  {/*<label htmlFor="events_dropdown">*/}
+                    Events
+                    <i className="down arrow"></i>
+                  {/*</label>*/}
+                  
+                  <ul className="sub_nav">
+                    <li className="sub_items">Previous Shows</li>
+                    <li className="sub_items">Fashion Show Tickets</li>
+                  </ul>
+                </li>
+            </a>
+            <a href="/volunteers">
+              <li className="nav_items">Volunteers
+                <i className="down arrow"></i>
+                <ul className="sub_nav">
+                  <a href="/designer_volunteers"><li className="sub_items">Designers</li></a>
+                  <a href="/model_volunteers"><li className="sub_items">Models</li></a>
+                  <a href="/administrator_volunteers"><li className="sub_items">Adiministrators</li></a>
+                </ul>
+              </li>
+            </a>
             <a href="/donation"><li className="nav_items">Donation</li></a>
             <a href="/contactus"><li className="nav_items">Contact Us</li></a>
           </ul>
