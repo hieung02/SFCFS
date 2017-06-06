586b31f6c1318fd6979a0fed8ec5e0b55aac04cf
Hieu <Hieu@Hieus-MacBook-Pro.local>
2017-06-06 16:40:58 -0700

    added paypal donation page

--- src/app/pages/donation.js
+++ src/app/pages/donation.js
@@ -11,14 +11,37 @@ export class Donation extends Component {
           <article className="application">
             <p className="app_icon_wrapper">
               <img src={img2} className="sub_icon"/>
-              <h4>Donate</h4>
-              Come to Our Fashion Show!!
+              <h4>Donate!</h4>
+              Make A Difference. Donate to the Scottish Rite Childhood Language Centers!
             </p>
             <div className="application_detail">
-              <div className="event_brite"><iframe src="//eventbrite.com/tickets-external?eid=26854686089&ref=etckt" frameBorder="0" height="431" width="100%" vspace="0" hspace="0" marginHeight="5" marginWidth="5" scrolling="auto" allowTransparency/>
-              </div>
-              <a href="https://www.eventbrite.com/e/sf-charity-fashion-show-2017-tickets-26854686089?ref=ebtn" target="_blank" rel="noopener noreferrer"><img src="https://www.eventbrite.com/custombutton?eid=26854686089" alt="Eventbrite - SF Charity Fashion Show 2017"/>
-              </a>
+              <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                  <input type="hidden" name="cmd" value="_s-xclick"/><br/>
+                  <input type="hidden" name="hosted_button_id" value="H7LXZM2BCSA6C"/>
+                  <table>
+                    <tbody>
+                      <tr>
+                      <td>
+                        <input type="hidden" name="on0" value="Donate directly to the charity:"/>Donate directly to the charity via PayPal:</td>
+                      </tr>
+                    <tr>
+                      <td>
+                        <select name="os0"><option value="Donation Amount">Select Donation Amount </option><option value="Option 1">Option 1 $25.00 USD</option><option value="Option 2">Option 2 $50.00 USD</option><option value="Option 3">Option 3 $75.00 USD</option><option value="Option 4">Option 4 $100.00 USD</option><option value="Option 5">Option 5 $250.00 USD</option><option value="Option 6">Option 6 $500.00 USD</option><option value="Option 7">Option 7 $1,000.00 USD</option></select>
+                      </td>
+                    </tr>
+                    <tr>
+                    <td><input type="hidden" name="on1" value="SF Charity Fashion Show"/>Support the San Francisco / Burlingame Clinics:</td>
+                    </tr>
+                    <tr>
+                    <td><select name="os1"><option value="San Francisco / Burlingame">San Francisco / Burlingame </option></select> </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                  <p><input type="hidden" name="currency_code" value="USD"/><br/>
+                  <input type="image" className="paypal_btn" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/><br/>
+                  <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"/><br/>
+                  </p>
+              </form>
             </div>
           </article>
         </section>
